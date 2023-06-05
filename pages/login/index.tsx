c2bfdde1e1405c2a0622ac008e97a4d93bd4be63
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-05 22:45:07 +0900

    ログイン画面の完成！

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { DefaultLayout } from "../../layout/DefaultLayout";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import { Grid, Typography, Button } from "@mui/material";
+import Link from "next/link";
 
-function LoginForm() {
+const textFieldStyle = {
+  width: "400px",
+  "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+    borderColor: "#683212",
+  },
+  "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+    borderColor: "#bb947c",
+  },
+  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: "#683212",
+  },
+};
+
+const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -15,10 +33,8 @@ function LoginForm() {
         password,
       });
       const token = response.data.token;
-
       // token を保存
       localStorage.setItem("token", token);
-
       // ログインに成功したら、ユーザーをホームページなどにリダイレクト
       window.location.replace("/");
     } catch (error) {
@@ -27,27 +43,104 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      {error && <div>{error}</div>}
-      <button type="submit">Login</button>
-    </form>
+    <DefaultLayout>
+      <Grid
+        container
+        direction="column"
+        sx={{ alignItems: "center", width: "100%", paddingTop: "80px" }}
+      >
+        <Typography variant="h4">ログイン</Typography>
+        <Grid item xs={12}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            autoComplete="off"
+          >
+            <Grid
+              item
+              sx={{ alignItems: "center", width: "100%", paddingTop: "40px" }}
+            >
+              <TextField
+                id="outlined-email"
+                label="メールアドレス"
+                variant="outlined"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                InputLabelProps={{
+                  style: { color: "#683212" },
+                }}
+                sx={textFieldStyle}
+              />
+            </Grid>
+            <Grid
+              item
+              sx={{
+                alignItems: "center",
+                width: "100%",
+                paddingTop: "20px",
+                paddingBottom: "10px",
+              }}
+            >
+              <TextField
+                id="outlined-password"
+                label="パスワード"
+                variant="outlined"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                InputLabelProps={{
+                  style: { color: "#683212" },
+                }}
+                sx={textFieldStyle}
+              />
+            </Grid>
+            <Grid sx={{ paddingTop: "10px" }}>
+              <span style={{ textDecoration: "underline" }}>
+                パスワードを忘れた場合
+              </span>
+            </Grid>
+            {error && (
+              <Grid sx={{ paddingTop: "20px" }}>
+                <Typography color="error">{error}</Typography>
+              </Grid>
+            )}
+            <Grid
+              item
+              sx={{ alignItems: "center", width: "100%", paddingTop: "30px" }}
+            >
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  backgroundColor: "#f59e0b",
+                  color: "#ffffff",
+                  "&:hover": {
+                    backgroundColor: "#f59f0bc5",
+                  },
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                }}
+              >
+                ログイン
+              </Button>
+            </Grid>
+            <Grid sx={{ paddingTop: "20px" }}>
+              <span>まだアカウントをお持ちでないですか？</span>
+              <Link href={"/login/Signup"}>
+                <span style={{ color: "#F59E0B", cursor: "pointer" }}>
+                  新規登録
+                </span>
+              </Link>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
+    </DefaultLayout>
   );
-}
+};
 
 export default LoginForm;
