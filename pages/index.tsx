d2fa9c025965f6ce74e466f9b5fd76b2675fb09e
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-06 13:05:25 +0900

    Topページのリファクタリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,67 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { DefaultLayout } from "../layout/DefaultLayout";
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Grid,
-  Typography,
-  Button,
-} from "@mui/material";
+import { Grid, Typography, Button } from "@mui/material";
 import Link from "next/link";
-import { relative } from "path";
 import { useAppDispatch } from "../redux/hook";
 import { resetAnswers } from "../redux/reducer/question";
 import React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Image from "next/image";
 
+const HomePageText = ({ children }) => (
+  <Typography variant="h6" sx={{ fontFamily: "HiraMinProN-W3" }}>
+    {children}
+  </Typography>
+);
+
+type HomePageButtonProps = {
+  href: string;
+  color: string;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+};
+
+const HomePageButton: React.FC<HomePageButtonProps> = ({
+  href,
+  color,
+  children,
+  style,
+}) => (
+  <Link href={href}>
+    <Button
+      variant="outlined"
+      sx={{
+        borderColor: color,
+        color: color,
+        "&:hover": {
+          backgroundColor: color,
+          color: "white",
+          borderColor: color,
+        },
+        fontSize: "18px",
+        fontWeight: "bold",
+        fontFamily: "HiraMinProN-W3",
+        ...style,
+      }}
+    >
+      {children}
+    </Button>
+  </Link>
+);
+
 const Home: NextPage = () => {
-  //resetの処理を宣言
   const dispatch = useAppDispatch();
+
   React.useEffect(() => {
-    //ページを開いた時にリセットする
     dispatch(resetAnswers());
   }, [dispatch]);
 
-  //レスポンシブ設定を定義
   const matches = useMediaQuery("(min-width:767px)");
 
   return (
     <DefaultLayout>
       {matches ? (
         <>
-          {/* PC表示 */}
           <Grid>
             <Image
               src="/images/top.png"
@@ -44,18 +71,16 @@ const Home: NextPage = () => {
               width="400px"
             />
             <Grid>
-              <Typography variant="h6" sx={{ fontFamily: "HiraMinProN-W3" }}>
+              <HomePageText>
                 ワインって種類が多すぎて何が自分に合うワインなのかわからない…
-              </Typography>
-              <Typography variant="h6" sx={{ fontFamily: "HiraMinProN-W3" }}>
-                そう思った経験はありませんか？
-              </Typography>
-              <Typography variant="h6" sx={{ fontFamily: "HiraMinProN-W3" }}>
+              </HomePageText>
+              <HomePageText>そう思った経験はありませんか？</HomePageText>
+              <HomePageText>
                 このサイトは10個の質問に答えるだけで、あなたにぴったりのワインを診断いたします！
-              </Typography>
-              <Typography variant="h6" sx={{ fontFamily: "HiraMinProN-W3" }}>
+              </HomePageText>
+              <HomePageText>
                 あなたにぴったりのワインを選んで、ワインライフをもっと楽しんでみませんか？
-              </Typography>
+              </HomePageText>
             </Grid>
             <Grid sx={{ paddingTop: "30px" }}>
               <Typography
@@ -77,46 +102,16 @@ const Home: NextPage = () => {
               </Typography>
             </Grid>
             <Grid sx={{ paddingTop: "30px" }}>
-              <Link href={"/shindan/aka"}>
-                <Button
-                  variant="outlined"
-                  sx={{
-                    borderColor: "#CD1919",
-                    color: "#CD1919",
-                    "&:hover": {
-                      backgroundColor: "#CD1919",
-                      color: "white",
-                      borderColor: "#CD1919",
-                    },
-                    fontSize: "18px",
-                    fontWeight: "bold",
-                    fontFamily: "HiraMinProN-W3",
-                    marginRight: "20px",
-                  }}
-                >
-                  赤ワインで診断
-                </Button>
-              </Link>
-              <Link href={"/shindan/shiro"}>
-                <Button
-                  variant="outlined"
-                  sx={{
-                    borderColor: "#10B981",
-                    color: "#10B981",
-                    "&:hover": {
-                      backgroundColor: "#10B981",
-                      color: "white",
-                      borderColor: "#10B981",
-                    },
-                    fontSize: "18px",
-                    fontWeight: "bold",
-                    fontFamily: "HiraMinProN-W3",
-                    marginLeft: "20px",
-                  }}
-                >
-                  白ワインで診断
-                </Button>
-              </Link>
+              <HomePageButton href={"/shindan/aka"} color="#CD1919">
+                赤ワインで診断
+              </HomePageButton>
+              <HomePageButton
+                href={"/shindan/shiro"}
+                color="#10B981"
+                style={{ marginLeft: "20px" }}
+              >
+                白ワインで診断
+              </HomePageButton>
             </Grid>
           </Grid>
         </>
