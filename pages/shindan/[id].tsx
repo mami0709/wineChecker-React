caf3a93d37a67aa468ec464102d32f49f9250b67
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-07 19:44:04 +0900

    おすすめワイン詳細ページの完成

--- pages/shindan/[id].tsx
+++ pages/shindan/[id].tsx
@@ -2,17 +2,11 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import { DefaultLayout } from "../../src/layout/DefaultLayout";
-import {
-  Card,
-  CardMedia,
-  Typography,
-  Button,
-  Container,
-  Box,
-  Link,
-} from "@mui/material";
+import { CardMedia, Typography, Container, Box, Grid } from "@mui/material";
 import axios from "axios";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { BasicButton } from "../components/Button/BasicButton";
+import { OutlineButton } from "../components/Button/outlineButton";
 
 const WineDetail: NextPage = () => {
   const router = useRouter();
@@ -44,16 +38,26 @@ const WineDetail: NextPage = () => {
 
   return (
     <DefaultLayout>
-      <Container fixed style={{ position: "relative" }}>
+      <Container
+        style={{
+          backgroundColor: "white",
+          paddingBottom: "60px",
+        }}
+      >
         <Box
           style={{
-            position: "absolute",
+            backgroundColor: "white",
             display: "flex",
-            left: "5%",
             width: "100%",
           }}
         >
-          <Card sx={{ height: "100vh", width: "50vh", margin: "10px" }}>
+          <Grid
+            sx={{
+              height: "80vh",
+              padding: "50px 130px",
+              margin: "10px",
+            }}
+          >
             <CardMedia
               component="img"
               image={wineData?.wine_image}
@@ -61,91 +65,136 @@ const WineDetail: NextPage = () => {
                 height: "100%",
                 width: "auto",
                 margin: "10px",
-                marginLeft: "120px",
               }}
             />
-          </Card>
-          <Card
-            sx={{
-              bgcolor: "#E6E6FA",
-              height: "100vh",
-              top: "25px",
-              width: "60vh",
-            }}
+          </Grid>
+          <Grid
             style={{
               display: "flex",
               flexDirection: "column",
-              marginTop: "10px",
               textAlign: "left",
-              paddingLeft: "20px",
-              paddingRight: "15px",
+              padding: "10px 40px 40px 20px",
             }}
           >
-            <Typography
-              variant="h4"
+            <Grid
               style={{
-                marginTop: 30,
-                marginBottom: 30,
-                fontWeight: "bold",
+                borderBottom: "1px solid #c5c5c5",
+                paddingBottom: "10px",
               }}
             >
-              {wineData?.wine_name}
-            </Typography>
-            <Box sx={{ borderBottom: 5, marginBottom: 5, color: "white" }} />
-            <Typography variant="h5">
-              【種類】
-              {wineData?.wine_type}
-            </Typography>
-            <Typography variant="h5" sx={{ marginTop: "10px" }}>
-              【おすすめ一言】{wineData?.one_word}
-            </Typography>
-            <Typography variant="h5" sx={{ marginTop: "10px" }}>
-              【原産国名】{wineData?.wine_country}
-            </Typography>
-            <Typography variant="h5" sx={{ marginTop: "10px" }}>
-              【産地】{wineData?.winery}
-            </Typography>
-            <Typography variant="h5" sx={{ marginTop: "10px" }}>
-              【ぶどう品種】{wineData?.breed}
-            </Typography>
-            <Typography variant="body1" sx={{ marginTop: "10px" }}>
-              【詳細】{wineData?.comment}
-            </Typography>
-            <Link
-              target="_blank"
-              href={wineData?.link}
-              sx={{
-                marginTop: "30px",
-                textAlign: "center",
-                textDecoration: "none",
-              }}
-            >
-              <Button
-                variant="contained"
-                sx={{
-                  marginTop: "10px",
-                  background: " rgb(243,208,120)",
-                  boxShadow: "0 1px 0 rgba(255,255,255,.4) inset",
-                  borderRadius: "3px",
-                  borderColor: "#a88734 #9c7e31 #846a29",
-                  borderStyle: "solid",
-                  borderWidth: "1px",
-                  width: "300px",
-                  height: "80px",
-                  color: "#111",
-                  fontSize: "1.2em",
+              <Typography
+                variant="h4"
+                style={{
+                  marginTop: 30,
+                  marginBottom: 30,
                   fontWeight: "bold",
-                  ":hover": {
-                    background: "#eeb933",
-                    color: "#222222",
-                  },
                 }}
               >
-                amazonで見てみる
-              </Button>
-            </Link>
-          </Card>
+                {wineData?.wine_name}
+              </Typography>
+              <Typography variant="h6">
+                {wineData?.english_wine_name}
+              </Typography>
+              {wineData?.years && (
+                <Typography variant="h6">{wineData?.years}年</Typography>
+              )}
+            </Grid>
+
+            <Grid
+              style={{
+                borderBottom: "1px solid #c5c5c5",
+                padding: "30px 0",
+              }}
+            >
+              <Typography variant="h6">{wineData?.comment}</Typography>
+            </Grid>
+
+            <Grid
+              style={{
+                padding: "30px 0",
+              }}
+            >
+              <Typography
+                variant="h6"
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                {wineData?.one_word}
+              </Typography>
+            </Grid>
+
+            <Grid
+              style={{
+                padding: "20px 0",
+              }}
+            >
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                商品情報
+              </Typography>
+              <Typography variant="h6">
+                タイプ
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.wine_type}
+                </Box>
+              </Typography>
+              <Typography variant="h6">
+                生産者
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.producer}
+                </Box>
+              </Typography>
+              <Typography variant="h6">
+                生産地
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.wine_country}
+                </Box>
+              </Typography>
+              <Typography variant="h6">
+                ワイナリー
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.winery}
+                </Box>
+              </Typography>
+              <Typography variant="h6">
+                品種
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.breed}
+                </Box>
+              </Typography>
+              <Typography variant="h6">
+                容量
+                <Box component="span" sx={{ paddingLeft: 3 }}>
+                  {wineData?.capacity} ml
+                </Box>
+              </Typography>
+            </Grid>
+          </Grid>
         </Box>
+        <Grid
+          container
+          justifyContent="center"
+          spacing={15}
+          sx={{ paddingTop: "30px" }}
+        >
+          <Grid item>
+            <OutlineButton
+              href={"/shindan/recommend"}
+              color="#F59E0B"
+              style={{ paddingLeft: "50px", paddingRight: "50px" }}
+            >
+              戻る
+            </OutlineButton>
+          </Grid>
+          <Grid item>
+            <BasicButton
+              href={wineData?.wine_url || "#"} // エラー回避
+              color="#F59E0B"
+            >
+              ショップで見てみる
+            </BasicButton>
+          </Grid>
+        </Grid>
       </Container>
     </DefaultLayout>
   );
